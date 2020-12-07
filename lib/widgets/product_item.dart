270b99b47b7f3607378cbe743741df258353001f
zagalapawel <zagalapawel@gmail.com>
2020-12-07 21:44:52 +0100

    display & data

---
+++ lib/widgets/product_item.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class ProductItem extends StatelessWidget {
+  final String id;
+  final String title;
+  final String imageUrl;
+
+  ProductItem(this.id, this.title, this.imageUrl);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: GridTile(
+        child: Image.network(imageUrl),
+      ),
+    );
+  }
+}

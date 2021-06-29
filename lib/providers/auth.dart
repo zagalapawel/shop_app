80228665a19a93b4de472d30a4a1efd71e420788
zagalapawel <zagalapawel@gmail.com>
2021-06-29 22:16:17 +0200

    error handling

--- lib/providers/auth.dart
+++ lib/providers/auth.dart
@@ -5,6 +5,8 @@ import 'dart:convert';
 import 'package:flutter/widgets.dart';
 import 'package:http/http.dart' as http;
 
+import '../models/http_exception.dart';
+
 class Auth with ChangeNotifier {
   String _token;
   DateTime _expiryDate;
@@ -12,18 +14,24 @@ class Auth with ChangeNotifier {
 
   Future<void> _authenticate(String email, String password, String urlSegment) async {
     Uri url = Uri.parse('https://identitytoolkit.googleapis.com/v1/accounts:$urlSegment?key=AIzaSyAsuam9fTUNOkWqUaT_wSjNTHP4iEeneIc');
-
-    final response = await http.post(
-      url,
-      body: json.encode(
-        {
-          'email': email.trimRight(),
-          'password': password,
-          'returnSecureToken': true,
-        },
-      ),
-    );
-    print(json.decode(response.body));
+    try {
+      final response = await http.post(
+        url,
+        body: json.encode(
+          {
+            'email': email.trimRight(),
+            'password': password,
+            'returnSecureToken': true,
+          },
+        ),
+      );
+      final responseData = json.decode(response.body);
+      if (responseData['error'] != null) {
+        throw HttpException(responseData['error']['message']);
+      }
+    } catch (error) {
+      throw error;
+    }
   }
 
   Future<void> signup(String email, String password) async {

8ca24deeb4d5d79145daa449e0c4a7a94ec58c45
zagalapawel <zagalapawel@gmail.com>
2021-06-29 21:40:19 +0200

    singup & login methods + authentication

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-import './screens/products_overview_screen.dart';
-import './screens/product_detail_screen.dart';
-import './screens/cart_screen.dart';
 import './providers/products.dart';
 import './providers/cart.dart';
 import './providers/orders.dart';
+import './providers/auth.dart';
+import './screens/cart_screen.dart';
+import './screens/products_overview_screen.dart';
+import './screens/product_detail_screen.dart';
 import './screens/orders_screen.dart';
 import './screens/user_products_screen.dart';
 import './screens/edit_product_screen.dart';
@@ -21,6 +22,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MultiProvider(
       providers: [
+        ChangeNotifierProvider.value(
+          value: Auth(),
+        ),
         ChangeNotifierProvider(
           create: (ctx) => Products(),
         ),
---
+++ lib/providers/auth.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/cupertino.dart';
+
+import 'dart:convert';
+
+import 'package:flutter/widgets.dart';
+import 'package:http/http.dart' as http;
+
+class Auth with ChangeNotifier {
+  String _token;
+  DateTime _expiryDate;
+  String _userId;
+
+  Future<void> _authenticate(String email, String password, String urlSegment) async {
+    Uri url = Uri.parse('https://identitytoolkit.googleapis.com/v1/accounts:$urlSegment?key=AIzaSyAsuam9fTUNOkWqUaT_wSjNTHP4iEeneIc');
+
+    final response = await http.post(
+      url,
+      body: json.encode(
+        {
+          'email': email.trimRight(),
+          'password': password,
+          'returnSecureToken': true,
+        },
+      ),
+    );
+    print(json.decode(response.body));
+  }
+
+  Future<void> signup(String email, String password) async {
+    return _authenticate(
+      email,
+      password,
+      'signUp',
+    );
+  }
+
+  Future<void> login(String email, String password) async {
+    return _authenticate(
+      email,
+      password,
+      'signInWithPassword',
+    );
+  }
+}
